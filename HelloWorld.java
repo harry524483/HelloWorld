2f4abede914d492c8a505b38a8db7d4ffed5606d
harry524483 <harinder.singh@sainsburys.co.uk>
2018-12-20 21:14:40 +0000

    change HelloWorld console message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] arg) {
 		for(int i =0 ; i < 10; i++) {
-			System.out.println("Hello World Again " + i);
+			System.out.println("Hello World " + i);
 		}
 	}
 }
